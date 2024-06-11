c5522d3b1d59898a94f6362f4f11086d1e0d312f
Gianmarco <118683146+gpeccii@users.noreply.github.com>
2024-06-11 12:29:51 +0200

    Add files via upload

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -8,33 +8,27 @@ int Account::_nbAccounts = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals = 0;
 
-int Account::getNbAccounts(void)
-{
+int Account::getNbAccounts(void) {
 	return (_nbAccounts);
 }
 
-int Account::getTotalAmount(void)
-{
+int Account::getTotalAmount(void) {
 	return (_totalAmount);
 }
 
-int Account::getNbDeposits(void)
-{
+int Account::getNbDeposits(void) {
 	return (_totalNbDeposits);
 }
-int Account::getNbWithdrawals(void)
-{
+int Account::getNbWithdrawals(void) {
 	return (_totalNbWithdrawals);
 }
 
-void Account::displayAccountsInfos(void)
-{
+void Account::displayAccountsInfos(void) {
 	_displayTimestamp();
 	std::cout << "accounts:" << _nbAccounts << "; total:" << _totalAmount << "; deposits:" << _totalNbDeposits << "; withdrawals:" << _totalNbWithdrawals << "\n";
 }
 
-Account::Account(int initial_deposit)
-{
+Account::Account(int initial_deposit) {
 	_displayTimestamp();
 	_nbAccounts++;
 	_accountIndex = _nbAccounts - 1;
@@ -44,14 +38,13 @@ Account::Account(int initial_deposit)
 	_nbWithdrawals = 0;
 	std::cout << "index:" << _accountIndex << "; amount:" << _amount << "; created\n";
 }
-Account::~Account(void)
-{
+
+Account::~Account(void) {
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << "; amount:" << this->_amount << "; closed\n";
 }
 
-void Account::makeDeposit(int deposit)
-{
+void Account::makeDeposit(int deposit) {
 	_displayTimestamp();
 	int p_amount = _amount;
 	_amount += deposit;
@@ -61,8 +54,7 @@ void Account::makeDeposit(int deposit)
 	std::cout << "index:" << this->_accountIndex << "; p_amount:" << p_amount << "; deposit:" << deposit << "; amount:" << checkAmount() << "; nb_deposits:" << _nbDeposits << "\n";
 }
 
-bool Account::makeWithdrawal(int withdrawal)
-{
+bool Account::makeWithdrawal(int withdrawal) {
 	if(checkAmount() - withdrawal < 0)
 	{
 		_displayTimestamp();
@@ -81,19 +73,17 @@ bool Account::makeWithdrawal(int withdrawal)
 		return (true);
 	}
 }
-int Account::checkAmount(void) const
-{
+
+int Account::checkAmount(void) const {
 	return (_amount);
 }
 
-void Account::displayStatus(void) const
-{
+void Account::displayStatus(void) const {
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << "; amount:" << this->_amount << "; deposits:" << _nbDeposits << "; withdrawals:" << _nbWithdrawals << "\n";
 }
 
-void Account::_displayTimestamp(void)
-{
+void Account::_displayTimestamp(void) {
 	std::time_t currentTime;
 	std::tm* timeInfo;
 	char buffer[20]; // Dimensione sufficiente per il timestamp nel formato richiesto
