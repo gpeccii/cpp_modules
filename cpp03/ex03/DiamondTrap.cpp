4e784932e411c3f2f57d9a5fab515f6a1a9160db
Gianmarco <118683146+gpeccii@users.noreply.github.com>
2024-06-11 15:57:00 +0200

    Add files via upload

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -1,11 +1,11 @@
 #include "DiamondTrap.hpp"
 
-DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), name(name) {
-	this->hitPoints = FragTrap::hitPoints;
-	this->energyPoints = ScavTrap::energyPoints;
-	this->attackDamage = FragTrap::attackDamage;
-	std::cout << "DiamondTrap " << name << " created with default constructor" << std::endl;
-}
+	DiamondTrap::DiamondTrap(std::string name) : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), name(name) {
+		this->hitPoints = FragTrap::hitPoints;
+		this->energyPoints = ScavTrap::energyPoints;
+		this->attackDamage = FragTrap::attackDamage;
+		std::cout << "DiamondTrap " << name << " created with default constructor" << std::endl;
+	}
 
 DiamondTrap::DiamondTrap(const DiamondTrap &toCopy) : ClapTrap(toCopy), ScavTrap(toCopy), FragTrap(toCopy), name(toCopy.name) {
 	std::cout << "DiamondTrap " << name << " has been created with copy constructor" << std::endl;
